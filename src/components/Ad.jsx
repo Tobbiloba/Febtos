2ac882e15b3aa0cec0642fc371f71c5f2d512c3f
Tobiloba <salauoluwatobiloba0@gmail.com>
2023-07-27 12:31:27 +0100

    added skeleton loading to ad

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react';
 import React from 'react';
-
+import Skeleton from '@mui/material/Skeleton';
 const Ad = ({text, image, buttonText, styles}) => {
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowContent(true)
+    }, [5000])
+  }, [])
   return (
-    <div className='padding mt-[1.5rem] '>
-      <div className='w-[100%] h-[10rem] rounded-2xl bg-orange-600 flex flex-row justify-between'>
+    <div className='padding mt-[1.5rem] h-[10rem]'>
+      {
+        showContent ? <div className='w-[100%] h-[10rem] rounded-2xl bg-orange-600 flex flex-row justify-between'>
         <div className='px-3 py-3 h-[100%] flex flex-col justify-evenly'>
           <h1 className='text-[19px] text-white font-mono font-bold'>Get your special <br /> sale up to 50%</h1>
           <button className='border px-3 py-1 rounded-2xl text-[16px] text-orange-600 font-bold bg-white w-fit'>Shop now!</button>
@@ -11,7 +20,13 @@ const Ad = ({text, image, buttonText, styles}) => {
         <div className='w-[40%] h-[100%] flex justify-center'>
           <img src="/bag.png" alt="bag" className='object-contain'/>
         </div>
-      </div>
+      </div> : <div className='relative bottom-[3.65rem]'>
+  <Skeleton sx={{ bgcolor: 'grey.500' }} animation="wave" height={265} className='border'/>
+  </div>
+      }
+      
+  
+
     </div>
   );
 }
---
+++ src/data/categories.js
@@ -0,0 +1,63 @@
+const categories = [
+    {
+      id: 1,
+      name: "Clothes",
+      description: "Explore a wide range of fashionable clothing for all ages.",
+      image: "https://oldnavy.gap.com/Asset_Archive/ONWeb/content/0029/745/739/assets/230501_12-M5093_B_DP_NewArrivals.jpg",
+    },
+    {
+      id: 2,
+      name: "Shoes",
+      description: "Discover stylish and comfortable shoes for every occasion.",
+      image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/c76e2119-acb7-4944-9085-d4f5ae2bda4a/go-flyease-easy-on-off-shoes-3svRCL.png",
+    },
+    {
+      id: 3,
+      name: "Gadgets",
+      description: "Shop the latest tech gadgets and accessories.",
+      image: "https://hips.hearstapps.com/hmg-prod/images/best-gadgets-64aeb579d3267.png",
+    },
+    {
+      id: 4,
+      name: "Foods",
+      description: "Find delicious and high-quality food products.",
+      image: "https://hips.hearstapps.com/hmg-prod/images/healthy-food-clean-eating-selection-royalty-free-image-854725370-1553880935.jpg?crop=0.910xw:0.688xh;0.00160xw,0&resize=1200:*",
+    },
+    {
+      id: 5,
+      name: "Home & Living",
+      description: "Enhance your living space with trendy home decor and essentials.",
+      image: "https://media.designcafe.com/wp-content/uploads/2020/09/30173843/home-decor-ideas-for-living-room.jpg",
+    },
+    {
+      id: 6,
+      name: "Beauty & Personal Care",
+      description: "Discover beauty products and personal care items.",
+      image: "https://techbullion.com/wp-content/uploads/2023/07/Beauty_20_26_20Personal_20Care4.jpg",
+    },
+    {
+      id: 7,
+      name: "Electronics",
+      description: "Explore a wide variety of electronic devices and appliances.",
+      image: "https://www.africa-business.com/features/images-africa/electronics-home-appliances.jpg",
+    },
+    {
+      id: 8,
+      name: "Books & Stationery",
+      description: "Get lost in the world of books and find all your stationery needs.",
+      image: "https://www.rankme1.com/wp-content/uploads/2019/12/stationery-books.jpeg",
+    },
+    {
+      id: 9,
+      name: "Sports & Outdoors",
+      description: "Find sports equipment and outdoor gear for your active lifestyle.",
+      image: "https://media.istockphoto.com/id/949190760/photo/various-sport-equipments-on-grass.jpg?s=612x612&w=0&k=20&c=7JHsUcAFO7Vm2hido5ULJn2FUcOYTfi06GVkgVcuI6I=",
+    },
+    {
+      id: 10,
+      name: "Toys & Games",
+      description: "Discover fun toys and engaging games for all ages.",
+      image: "https://as2.ftcdn.net/v2/jpg/02/59/51/87/1000_F_259518799_VewJAVP7Bm2MaDD71ennTIaYayFFjkJ4.jpg",
+    },
+  ];
+  
