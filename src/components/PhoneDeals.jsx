c32209fdecb7945fbef9536c2683e56336b301bb
Tobiloba <salauoluwatobiloba0@gmail.com>
2023-07-28 12:04:05 +0100

    top deals

--- src/components/PhoneDeals.jsx
+++ src/components/PhoneDeals.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { phoneDeals } from '../data/phoneDeal';
 const PhoneDeals = () => {
   return (
-    <div className='bg-white'>
+    <div className='bg-white mb-[4rem]'>
       <div className='bg-slate-400 px-[1.5rem] py-[.25rem] text-center'>
             <h1 className='text-white text-[18px] font-bold font-mono'>Phone Deals</h1>
         </div>
         <div className='flex flex-row flex-wrap justify-between padding'>
         {
             phoneDeals.map((item) => (
-                <div key={item.id} className='mt-[1rem] rounded-md overflow-hidden'>
+                <div key={item.id} className='mt-[1rem] rounded-md overflow-hidden w-[30%]'>
                     <img src={item.image} alt="phones" className='w-[7rem] h-[8.5rem]'/>
                     <div className=' h-2 relative bottom-[1rem] '>
                         <h1 className='backdrop-blur-sm bg-black/70 text-white text-[12px] py-1 text-center font-mono font-bold rounded-b-md'>{item.name}</h1>
