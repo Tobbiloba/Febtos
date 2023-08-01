4fd298a53b3b39a66c04cfdc21c35fafe93cd845
Tobiloba <salauoluwatobiloba0@gmail.com>
2023-08-01 14:19:41 +0100

    product details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
-
+// AiOutlineCaretLeft
+// AiOutlineHeart
+// AiOutlineShareAlt
+import {AiOutlineShareAlt, AiOutlineHeart, AiOutlineLeft} from 'react-icons/ai'
 const ProductDetails = () => {
   return (
-    <div>
-      product details
+    <div className='px-[20px] py-[1rem] bg-orange-100 h-[100vh]'>
+      <div className='absolute text-[24px] top-[1rem] px-[1rem] w-[100vw] left-0 flex justify-between items-center'>
+        <div className='p-2 rounded-full shadow-md text-slate-600 bg-slate-50 hover:bg-slate-200 '>
+            <AiOutlineLeft />
+        </div>
+            <div className='flex flex-row gap-[2rem]'>
+            <div className='p-2 rounded-full shadow-md text-slate-600 bg-slate-50 hover:bg-slate-200 '>
+                <AiOutlineHeart />
+            </div>
+            <div className='p-2 rounded-full shadow-md text-slate-600 bg-slate-50 hover:bg-slate-200'>
+                <AiOutlineShareAlt />
+            </div>
+            </div>
+        </div>
+       <div>
+        <div></div>
+        <div>
+        <div></div>
+        <div></div>
+        </div>
+        <div></div>
+      </div>
+
+      <div></div>
     </div>
   );
 }
