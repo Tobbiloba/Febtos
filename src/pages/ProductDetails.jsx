0ba3539151098573be7236c1117b9a9241a28a01
Tobiloba <salauoluwatobiloba0@gmail.com>
2023-08-01 14:20:56 +0100

    made some changes

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 // AiOutlineHeart
 // AiOutlineShareAlt
 import {AiOutlineShareAlt, AiOutlineHeart, AiOutlineLeft} from 'react-icons/ai'
+import { Link } from 'react-router-dom';
 const ProductDetails = () => {
   return (
     <div className='px-[20px] py-[1rem] bg-orange-100 h-[100vh]'>
       <div className='absolute text-[24px] top-[1rem] px-[1rem] w-[100vw] left-0 flex justify-between items-center'>
         <div className='p-2 rounded-full shadow-md text-slate-600 bg-slate-50 hover:bg-slate-200 '>
+            <Link to='/'>
             <AiOutlineLeft />
+            </Link>
+            
         </div>
             <div className='flex flex-row gap-[2rem]'>
             <div className='p-2 rounded-full shadow-md text-slate-600 bg-slate-50 hover:bg-slate-200 '>
@@ -19,7 +23,7 @@ const ProductDetails = () => {
             </div>
             </div>
         </div>
-       <div>
+       <div className=''>
         <div></div>
         <div>
         <div></div>
