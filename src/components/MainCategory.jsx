48a21148bfff7e025e4d2f77accfbe18151168ab
Tobiloba <salauoluwatobiloba0@gmail.com>
2023-08-02 18:02:17 +0100

    slider

--- src/components/MainCategory.jsx
+++ src/components/MainCategory.jsx
@@ -5,7 +5,7 @@ const MainCategory = () => {
     <div className='bg-white mt-[2rem] px-[20px] grid grid-cols-4 gap-4 pt-[2rem]'>
       {
         mainCategory.map((item) => (
-            <div>
+            <div key={item.id}>
                 <img src={item.img} alt="product" className='border w-[5rem] h-[4rem]'/>
                 <h1 className='text-center text-[12px] mt-1 font-sans font-light'>{item.title}</h1>
             </div>
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -10,13 +10,11 @@ import '../styles/showNav.css'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
+
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-// import './styles.css';
 
-// import required modules
 import { Pagination } from 'swiper/modules';
 
 const ProductDetails = () => {
